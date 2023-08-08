111d818549404090a206bc2a424f30b9732da5be
Moyseys Ferreira Veroni <99370603+Moyseys@users.noreply.github.com>
2023-08-08 19:46:34 -0300

    Single responsability principle

---
+++ src/SRP - (Single responsibility principle)/shopping-cart.ts
@@ -0,0 +1,78 @@
+// Ainda será refatorado
+
+interface Product {
+  nome: string;
+  price: number;
+}
+
+class ShoppingCart {
+  private readonly _itens: Product[] = [];
+  private _order: 'closed' | 'open' = 'open';
+  constructor() {}
+
+  public addItem(item: Product): void {
+    this._itens.push(item);
+  }
+
+  public removeItem(index: number): void {
+    this._itens.splice(index, 1);
+  }
+
+  public total(): number {
+    const total = +this._itens
+      .reduce((ac, item) => ac + item.price, 0)
+      .toFixed(2);
+
+    return total;
+  }
+
+  public checkout(): void {
+    if (this._order === 'closed' || this.isEmpty()) {
+      return console.log(
+        'Não foi possível finalizar o pedido, pois seu carrinho está fechado ou não possui itens',
+      );
+    }
+
+    this._order = 'closed';
+    this.sendMessage('Seu pedido foi enviado');
+    this.save();
+    this.clear();
+  }
+
+  private clear(): void {
+    this._itens.length = 0;
+  }
+
+  private sendMessage(msg: string): void {
+    return console.log(msg);
+  }
+
+  private save(): void {
+    return console.log('Seu pedido foi salvo');
+  }
+
+  private isEmpty(): boolean {
+    return this._itens.length <= 0;
+  }
+
+  //Usando um Generic padrão do type script para retornar um obj com propriedades com readonly
+  get item(): Readonly<Product[]> {
+    return this._itens;
+  }
+
+  get order(): 'open' | 'closed' {
+    return this.order;
+  }
+}
+
+const cart = new ShoppingCart();
+cart.addItem({ nome: 'mouse', price: 20 });
+cart.addItem({ nome: 'Teclado', price: 250.5 });
+cart.addItem({ nome: 'NoteBook', price: 3700 });
+
+const total = cart.total();
+
+console.log('Total: ' + total);
+cart.checkout();
+
+console.log(cart.item);
